c346cb6004fe575643e4d73075eb2c373bf49583
Pham Lien Sanh <18520146@gm.uit.edu.vn>
2021-01-19 14:23:52 +0700

    tt

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,9 +1,15 @@
 export default {
-  apiKey: "AIzaSyBFgtPDbNuClSeU7Am34W3rDtcSriG1oLE",
-  authDomain: "superselfapp.firebaseapp.com",
-  databaseURL: "https://superselfapp.firebaseio.com",
-  projectId: "superselfapp",
-  storageBucket: "superselfapp.appspot.com",
-  messagingSenderId: "975675020366",
-  appId: "1:975675020366:web:df2050d3a0549d1dc41f1f",
+  // apiKey: "AIzaSyBFgtPDbNuClSeU7Am34W3rDtcSriG1oLE",
+  // authDomain: "superselfapp.firebaseapp.com",
+  // databaseURL: "https://superselfapp.firebaseio.com",
+  // projectId: "superselfapp",
+  // storageBucket: "superselfapp.appspot.com",
+  // messagingSenderId: "975675020366",
+  // appId: "1:975675020366:web:df2050d3a0549d1dc41f1f",
+  apiKey: "AIzaSyCzJNltRK9g6SvoWHyzuNH79z3vPTYbkMk",
+  authDomain: "superselftest.firebaseapp.com",
+  projectId: "superselftest",
+  storageBucket: "superselftest.appspot.com",
+  messagingSenderId: "267720981117",
+  appId: "1:267720981117:web:51947f156a295775244f14"
 };
--- src/screens/SetupChallenge.js
+++ src/screens/SetupChallenge.js
@@ -26,6 +26,52 @@ import {
   import DateTimePicker from "@react-native-community/datetimepicker";
   import Colors from "../utils/Colors";
   
+  //NOTIFICATION IMPORT
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import {useEffect, useRef } from 'react';
+
+//NOTIFICATION SETUP
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      // alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    // alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
+
   export default function SetupChallengeScreen({route, navigation}) {
     const challengeSelected = route.params;
     const [user, setUser] = useContext(UserContext);
@@ -43,8 +89,25 @@ import {
     const [isModalStartDate, setIsModalStartDate] = useState(false);
     const [isModalSuccess, setIsModalSuccess] = useState(false);
 
+    const notificationListener = useRef();
+    const responseListener = useRef();
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+
     const challenge = useContext(ChallengeFirebaseContext);
+    
+    useEffect(() => {
+      registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
   
+      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+      });
+  
+      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+      });
+    });
+
     const renderImageGoal = () => (
       <Image
         source={require("../utils/superself-icon.png")}
@@ -65,15 +128,24 @@ import {
     };
 
     const setupChallenge = () => {
-      setIsModalSuccess(true);
-      
+      setIsModalSuccess(true);     
+      schedulePushNotification();
     } 
 
-    const handlechangeGoal = (event) =>{
-      const {name, type, value} = event.nativeEvent;
-      setGoal(value);
-      console.log(value);
-    }
+    //NOTIFICATION ADD A NOTIFICATION
+    async function schedulePushNotification() {
+    const triger = new Date(startdate.getDate() + reminders.getHours*60*60 + reminders.getMinutes * 60);
+    console.log(startdate.getDate());
+    await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "📬",
+      body:"Chúc bạn ngày mới vui vẻ! Hãy thực hiện và đánh dấu tiến độ khi xong nhé!" ,
+      data: { data: 'goes here' },
+    },
+    trigger,
+  });
+  console.log("Hô");
+  }
     const MyAvatar = user.profilePhotoUrl;
     return (
       <ScrollView
